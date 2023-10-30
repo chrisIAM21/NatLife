7f838330c1c5578bcb3af8ca7fa5805f175c9d4d
chrisIAM21 <christopher.andrade4604@alumnos.udg.mx>
2023-10-29 21:08:11 -0600

    Eva_theme_config

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import React from 'react';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { mapping, light as theme } from '@eva-design/eva';
+
+const MainApp = () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider mapping={mapping} theme={theme}>
+      <App />
+    </ApplicationProvider>
+  </>
+);
+
+AppRegistry.registerComponent(appName, () => MainApp);
